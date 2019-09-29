5ba86bedecebf3a0ea8b3c20116550be7d1368d6
MarceloLeite2604 <marceloleite2604@gmail.com>
2019-09-28 23:13:50 -0300

    Add "apple-and-orange" solution.

---
+++ apple-and-orange/src/main/java/Solution.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+    	int[] samHouseRange = {s, t};
+    	int appleTreePoint = a;
+    	int orangeTreePoint = b;
+    	
+    	int applesInRange = fruitsInRange(samHouseRange, appleTreePoint, apples);
+    	int orangesInRange = fruitsInRange(samHouseRange, orangeTreePoint, oranges);
+    	System.out.println(applesInRange);
+    	System.out.println(orangesInRange);
+    }
+
+	private static int fruitsInRange(int[] samHouseRange, int treePoint, int[] fruitPoints) {
+		int result = 0;
+    	for (int fruitPoint : fruitPoints) {
+			if ( inRange(samHouseRange, treePoint + fruitPoint) ) {
+				result++;
+			}
+		}
+    	
+    	return result;
+	}
+
+    private static boolean inRange(int[] range, int value) {
+    	return value >= range[0] && value <= range[1];
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scanner.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0]);
+
+        int t = Integer.parseInt(st[1]);
+
+        String[] ab = scanner.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0]);
+
+        int b = Integer.parseInt(ab[1]);
+
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        int[] apples = new int[m];
+
+        String[] applesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesItems[i]);
+            apples[i] = applesItem;
+        }
+
+        int[] oranges = new int[n];
+
+        String[] orangesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesItems[i]);
+            oranges[i] = orangesItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        scanner.close();
+    }
+}
