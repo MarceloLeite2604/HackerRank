3cbb142f6c9888abcfef21956514f0f12cf4d90e
MarceloLeite2604 <marceloleite2604@gmail.com>
2019-12-09 08:20:20 -0300

    Add "strange-advertising" solution

---
+++ strange-advertising/strangeadvertising.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the viralAdvertising function below.
+func viralAdvertising(n int32) int32 {
+	shared := 5
+	cummulative := 0
+
+	for i := int32(0); i < n; i++ {
+		liked := shared / 2
+
+		cummulative += liked
+
+		shared = liked * 3
+	}
+
+	return int32(cummulative)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	result := viralAdvertising(n)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
