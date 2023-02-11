bf207d71976111339111ca659280c7ce9c166c7f
MarceloLeite2604 <marceloleite2604@gmail.com>
2023-02-10 23:29:47 -0300

    Add "running-time" solution

---
+++ running-time/src/main/java/Solution.java
@@ -0,0 +1,55 @@
+    import java.io.*;
+    import java.util.*;
+    import java.util.stream.*;
+
+    import static java.util.stream.Collectors.joining;
+    import static java.util.stream.Collectors.toList;
+
+    class Result {
+
+        /*
+         * Complete the 'insertionSort2' function below.
+         *
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. INTEGER_ARRAY arr
+         */
+
+        public static void insertionSort2(int n, List<Integer> arr) {
+            var shifts = 0;
+            for (int endIndex = 1; endIndex < arr.size(); endIndex++) {
+                shifts += partialSort(endIndex, arr);
+            }
+            System.out.println(shifts);
+        }
+
+        private static int partialSort(int checkedIndex, List<Integer> arr) {
+            var shifts = 0;
+            for (int index = checkedIndex; index > 0; index--) {
+                if (arr.get(index) < arr.get(index - 1)) {
+                    shifts++;
+                    arr.add(index - 1, arr.remove(index));
+                }
+            }
+            return shifts;
+        }
+    }
+
+    public class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            int n = Integer.parseInt(bufferedReader.readLine()
+                    .trim());
+
+            List<Integer> arr = Stream.of(bufferedReader.readLine()
+                            .replaceAll("\\s+$", "")
+                            .split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+            Result.insertionSort2(n, arr);
+
+            bufferedReader.close();
+        }
+    }
