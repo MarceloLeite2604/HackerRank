2872dd59d512c13f7bc01d5066b33251788f4be2
MarceloLeite2604 <marceloleite2604@gmail.com>
2024-05-10 22:25:43 -0300

    Add "flipping-bits" solution

---
+++ flipping-bits/src/main/java/Solution.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.stream.IntStream;
+
+class Result {
+
+    /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long flippingBits(long n) {
+        long mask = 0x80000000L;
+        long result = 0;
+
+        while(Long.compareUnsigned(mask, 0) > 0) {
+            long value = (n & mask) == mask ? 0 : mask;
+            result += value;
+            mask >>>= 1;
+        }
+
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = Result.flippingBits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
